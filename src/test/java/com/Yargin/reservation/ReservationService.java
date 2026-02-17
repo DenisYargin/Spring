e8e9170c7376ba3b3c448b2e8215a57151e15201
denis <deiderd@mail.ru>
2026-02-17 20:16:09 +0300

    add Controller

---
+++ src/test/java/com/Yargin/reservation/ReservationService.java
@@ -0,0 +1,10 @@
+package com.Yargin.reservation;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReservationService {
+    public String getReservationById() {
+        return "reservation-1";
+    }
+}
