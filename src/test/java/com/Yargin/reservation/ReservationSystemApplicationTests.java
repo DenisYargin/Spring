c1a6bdb5dc783d661f344d03f7ceb4e0a2321952
denis <deiderd@mail.ru>
2026-02-17 19:40:55 +0300

    inti commit

---
+++ src/test/java/com/Yargin/reservation/ReservationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Yargin.reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
