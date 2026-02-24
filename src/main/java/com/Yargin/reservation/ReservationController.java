195145cf7ff63cd3c1e786d23abe06761ef55aa8
denis <deiderd@mail.ru>
2026-02-24 20:57:25 +0300

    add log

--- src/main/java/com/Yargin/reservation/ReservationController.java
+++ src/main/java/com/Yargin/reservation/ReservationController.java
@@ -1,5 +1,7 @@
 package com.Yargin.reservation;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,20 +12,22 @@ import java.util.List;
 public class ReservationController {
     private final ReservationService reservationService;
 
+    private static final Logger log = LoggerFactory.getLogger(ReservationController.class);
+
     public ReservationController(ReservationService reservationService) {
         this.reservationService = reservationService;
     }
 
     @GetMapping("/{id}")
     public Reservation getReservationById(@PathVariable Long id) {
-        System.out.println("Контроллер вызван!");
+        log.info("Вызвался метод getReservationById id=" + id);
         return reservationService.getReservationById(id);
 
     }
 
     @GetMapping()
     public List<Reservation> getAllReservation() {
-        System.out.println("Контроллер вызван!");
+        log.info("Вызвался метод getAllReservation");
         return reservationService.findAllReservation();
 
     }
