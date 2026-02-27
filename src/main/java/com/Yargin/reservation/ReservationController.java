f2d47d4cba9c6d4b4d63ae2c821eb20c1d2f1b35
denis <deiderd@mail.ru>
2026-02-27 21:23:45 +0300

    1)добавил crud методы: get по id, create(post), put, delete 2)добавил обработку и проверку на пересечении дат, и запрос/обновление/удалении по Id

--- src/main/java/com/Yargin/reservation/ReservationController.java
+++ src/main/java/com/Yargin/reservation/ReservationController.java
@@ -2,13 +2,16 @@ package com.Yargin.reservation;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @RestController
+@RequestMapping("/reservation")
 public class ReservationController {
     private final ReservationService reservationService;
 
@@ -19,18 +22,65 @@ public class ReservationController {
     }
 
     @GetMapping("/{id}")
-    public Reservation getReservationById(@PathVariable Long id) {
+    public ResponseEntity<Reservation> getReservationById(@PathVariable Long id) {
         log.info("Вызвался метод getReservationById id=" + id);
-        return reservationService.getReservationById(id);
-
+        try {
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body(reservationService.getReservationById(id));
+        } catch (NoSuchElementException e) {
+            return ResponseEntity.status(404).build();
+        }
     }
 
     @GetMapping()
-    public List<Reservation> getAllReservation() {
+    public ResponseEntity<List<Reservation>> getAllReservation() {
         log.info("Вызвался метод getAllReservation");
-        return reservationService.findAllReservation();
+        return ResponseEntity.ok(reservationService.findAllReservation());
+      //  return reservationService.findAllReservation();
 
     }
 
+    @PostMapping()
+    public ResponseEntity<Reservation> createReservation(
+           @RequestBody Reservation reservationToCreate
+    ) {
+        log.info("Вызвался метод createReservation");
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .header("test-header", "value-header")
+                .body(reservationService.createReservation(reservationToCreate));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Reservation> updateReservation(
+            @PathVariable Long id,
+            @RequestBody Reservation reservationToUpdate
+    ) {
+        log.info("Вызвался метод updateReservation id={}, reservationToUpdate={}",id, reservationToUpdate);
+        var updated = reservationService.updateReservation(id, reservationToUpdate);
+        return ResponseEntity.ok(updated);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteReservation(
+            @PathVariable("id") Long id
+    ) {
+        log.info("Вызвался метод deleteReservation id={}", id);
+        try{
+            reservationService.deleteReservation(id);
+            return ResponseEntity.ok()
+                    .build();
+        } catch (NoSuchElementException e){
+            return ResponseEntity.status(404).build();
+        }
+    }
+
+    @PostMapping("/{id}/approve")
+    public ResponseEntity<Reservation> approveReservation(
+            @PathVariable("id") Long id
+    ) {
+        log.info("Вызвался метод approveReservation id={}", id);
+        var reservation = reservationService.approveReservation(id);
+        return ResponseEntity.ok(reservation);
+    }
 
 }
